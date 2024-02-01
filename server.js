93ea478d667d745a80293b49903a6a445e1745e6
rajkaranxgill <irajkarangill@gmail.com>
2024-01-31 19:54:42 -0500

    final commit

--- server.js
+++ server.js
@@ -1,17 +1,46 @@
-const express = require('express')
-const path = require('path')
-const app = express() //create an express application
+const express = require('express');
+const path = require('path');
+const storeService = require('./store-service');
+const app = express();
 
-const HTTP_PORT = process.env.PORT || 8080; // assign a port
+const HTTP_PORT = process.env.PORT || 8080;
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'));  
+    res.redirect('/about');
 });
 
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/about.html'));
 });
 
-//Listen on port number
-app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+app.get('/shop', (req, res) => {
+    storeService.getPublishedItems()
+        .then(data => res.json(data))
+        .catch(error => res.json({ message: error }));
+});
+
+app.get('/items', (req, res) => {
+    storeService.getAllItems()
+        .then(data => res.json(data))
+        .catch(error => res.json({ message: error }));
+});
+
+app.get('/categories', (req, res) => {
+    storeService.getCategories()
+        .then(data => res.json(data))
+        .catch(error => res.json({ message: error }));
+});
+
+app.use((req, res) => {
+    res.status(404).send("Page Not Found");
+});
+
+storeService.initialize()
+    .then(() => {
+        app.listen(HTTP_PORT, () => console.log(`Express http server listening on port ${HTTP_PORT}`));
+    })
+    .catch((error) => {
+        console.error(`Initialization failed: ${error}`);
+    });
--- store-service.js
+++ store-service.js
@@ -1,62 +1,66 @@
 const fs = require('fs');
+const path = require('path');
 
-var items = [];
-var categories = [];
+let items = [];
+let categories = [];
 
-module.exports.initialize = function()
-{
-    return new Promise(function(resolve, reject)
-    {
-        try
-        {
-            fs.readFile('./data/items.json', function(err, data)
-            {
-                if(err) throw err;
-                items = JSON.parse(data);
+module.exports.initialize = function () {
+    return new Promise((resolve, reject) => {
+        try {
+            const itemsPath = path.join(__dirname, 'data/items.json');
+            const categoriesPath = path.join(__dirname, 'data/categories.json');
+
+            fs.readFile(itemsPath, 'utf8', (err, data) => {
+                if (err) {
+                    reject("Unable to read items.json file");
+                } else {
+                    items = JSON.parse(data);
+                    resolve("Success to read items.json file");
+                }
             });
-            fs.readFile('./data/categories.json', function(err,data)
-            {
-                if(err) throw err;
-                categories = JSON.parse(data);                
+
+            fs.readFile(categoriesPath, 'utf8', (err, data) => {
+                if (err) {
+                    reject("Unable to read categories.json file");
+                } else {
+                    categories = JSON.parse(data);
+                    resolve("Success to read categories.json file");
+                }
             });
-        } catch(e) {
-            reject("unable to read json file");
+        } catch (e) {
+            reject("Unable to read JSON files");
         }
-        resolve("success to read json file");
     });
 };
 
-
 module.exports.getAllItems = function () {
-    return new Promise(function (resolve, reject) {
-      if (items.length === 0) {
-        reject("No items returned");
-      } else {
-        resolve(items.slice());
-      }
+    return new Promise((resolve, reject) => {
+        if (items.length === 0) {
+            reject("No items returned");
+        } else {
+            resolve(items.slice());
+        }
     });
-  };
-  
+};
 
-  module.exports.getPublishedItems = function () {
-    return new Promise(function (resolve, reject) {
-      const publishedItems = items.filter(item => item.published === true);
-  
-      if (publishedItems.length === 0) {
-        reject("No published items returned");
-      } else {
-        resolve(publishedItems.slice()); 
-      }
-    });
-  };
-  
-  module.exports.getCategories = function () {
-    return new Promise(function (resolve, reject) {
-      if (categories.length === 0) {
-        reject("No categories returned");
-      } else {
-        resolve(categories.slice());
-      }
-    });
-  };
+module.exports.getPublishedItems = function () {
+    return new Promise((resolve, reject) => {
+        const publishedItems = items.filter(item => item.published === true);
 
+        if (publishedItems.length === 0) {
+            reject("No published items returned");
+        } else {
+            resolve(publishedItems.slice());
+        }
+    });
+};
+
+module.exports.getCategories = function () {
+    return new Promise((resolve, reject) => {
+        if (categories.length === 0) {
+            reject("No categories returned");
+        } else {
+            resolve(categories.slice());
+        }
+    });
+};
